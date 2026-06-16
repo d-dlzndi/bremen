9a2d4832ba47eca96f4f56a0e4d24e9aa8af5157
D-DL <d.dlzndi@gmail.com>
2026-06-17 00:02:59 +0900

    first commit

---
+++ enemy/enemy.gd
@@ -0,0 +1,47 @@
+extends CharacterBody3D
+
+@export var speed = 14
+@export var crouch_speed = 5
+@export var fall_acceleration = 75
+@export var jump_impulse = 20
+
+var target_velocity = Vector3.ZERO
+
+@export var pivot: Node3D
+
+@export var navigation_agent_3d: NavigationAgent3D
+
+
+func _physics_process(_delta: float) -> void:
+	
+	velocity = target_velocity
+	move_and_slide()
+
+func move(_delta : float):
+	var direction = Vector3.ZERO
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_back"):
+		direction.z += 1
+	if Input.is_action_pressed("move_forward"):
+		direction.z -= 1
+	
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		target_velocity.y = jump_impulse
+	
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
+		pivot.basis = Basis.looking_at(direction)
+	
+	var _speed : float = speed
+	scale.y = 1
+	if is_on_floor() and Input.is_action_pressed("crouch"):
+		_speed = crouch_speed
+		scale.y = 0.4
+	target_velocity.x = direction.x * _speed
+	target_velocity.z = direction.z * _speed
+	if not is_on_floor():
+		target_velocity.y = target_velocity.y - (fall_acceleration * _delta)
+	
